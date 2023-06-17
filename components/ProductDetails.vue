6e31ec925972d2da60e1f171b7740ac12ecb8b89
wiekusviljoen <wiekusviljoen@gmail.com>
2023-06-17 20:31:56 +0200

    commit on master

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="card ">
-        <div class="grid grid-cols-2 gap-10">
-            <div class="p-7">
-                <img :src="product.image" alt="product img" class=",x-auto my-7">
-            </div>
-            <div class="p-7">
-                <h2 class="text-4xl my-7">{{ product.title }}</h2>
-                <p class="text-xl my-7">Price - ${{ product.price }}</p>
-                <h3 class="font-bold border-b-2 pb-2">Product Description:</h3>
-                <p class="mb-7">{{ product.description }}</p>
+    <div class="background">
+        <div class="card ">
+            <div class="grid grid-cols-2 ">
+                <div class="p-7 ml-10">
+                    <img :src="product.image" alt="product img" class=",x-auto my-7">
+                </div>
+                <div class="p-7">
+                    <h2 class="text-4xl my-7">{{ product.title }}</h2>
+                    <p class="text-xl my-7">Price - ${{ product.price }}</p>
+                    <h3 class="font-bold border-b-2 pb-2">Product Description:</h3>
+                    <p class="mb-7">{{ product.description }}</p>
+                </div>
             </div>
         </div>
     </div>
@@ -19,8 +21,15 @@ const { product } = defineProps(['product'])
 </script>
 
 <style  scoped>
+.background {
+    background-color: white;
+    border-radius: 15px;
+}
+
 img {
     border-radius: 15px;
     max-width: 300px;
+
+
 }
 </style>
