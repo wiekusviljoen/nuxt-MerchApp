2c061c3389614ab1c1710e572c17dcbeb497594f
wiekusviljoen <wiekusviljoen@gmail.com>
2023-06-17 15:26:39 +0200

    commit on master

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,8 +1,19 @@
 <template>
     <div>
         <header class="shadow-sm bg-white">
-            <nav class="container mx-auto p=4">
+            <nav class="container mx-auto p-4 flex justify-between">
                 <NuxtLink to="/" class="font-bold">Dojo Merch</NuxtLink>
+                <ul class="flex gap-4">
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/products">Products</NuxtLink>
+                    </li>
+                </ul>
 
             </nav>
         </header>
